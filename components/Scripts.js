e3edb2e1c1545249e92931976587cf101edcbcc6
ming <matwming114@gmail.com>
2024-09-29 23:16:54 +1000

    add id to clarity

--- components/Scripts.js
+++ components/Scripts.js
@@ -30,7 +30,7 @@ const Scripts = () => {
       )}
       {BLOG.analytics && BLOG.analytics.provider === 'clarity' && (
         <>
-        <Script strategy={"lazyOnload"}>
+        <Script strategy={"lazyOnload"} id={'clarity'}>
           {` (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
