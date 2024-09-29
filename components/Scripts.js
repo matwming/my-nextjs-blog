f406f0452f2e972951bf72fdc6903252d5c3d4dc
ming <matwming114@gmail.com>
2024-09-29 23:02:52 +1000

    add clarity analytics

--- components/Scripts.js
+++ components/Scripts.js
@@ -28,6 +28,17 @@ const Scripts = () => {
           </Script>
         </>
       )}
+      {BLOG.analytics && BLOG.analytics.provider === 'clarity' && (
+        <>
+        <Script strategy={"lazyOnload"}>
+          {` (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${BLOG.analytics.clarity.projectId}");`}
+        </Script>
+        </>
+      )}
     </>
   )
 }
